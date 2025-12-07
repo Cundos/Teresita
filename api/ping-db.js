52101196b3ce14e9e41f067d7485d53f1243f897
Cundos <36012685+Cundos@users.noreply.github.com>
2025-12-07 19:55:01 -0300

    Create ping-db.js

---
+++ api/ping-db.js
@@ -0,0 +1,20 @@
+// api/ping-db.js
+const { getPool } = require("./db.js");
+
+module.exports = async (req, res) => {
+  try {
+    const pool = getPool();
+    const { rows } = await pool.query("SELECT now() as ahora, version()");
+    return res.status(200).json({
+      ok: true,
+      ahora: rows[0].ahora,
+      version: rows[0].version
+    });
+  } catch (err) {
+    console.error("Error en ping-db:", err);
+    return res.status(500).json({
+      ok: false,
+      error: err.message
+    });
+  }
+};
